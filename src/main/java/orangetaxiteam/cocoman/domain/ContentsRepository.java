d3ff3a6bdc19674df158edaf504b1f356f1e24f9
Gunhee Jang <ghj5719@naver.com>
2021-03-01 19:40:50 +0900

    feat: impl contents detail and init dummy data

--- src/main/java/orangetaxiteam/cocoman/domain/ContentsRepository.java
+++ src/main/java/orangetaxiteam/cocoman/domain/ContentsRepository.java
@@ -1,10 +1,19 @@
 package orangetaxiteam.cocoman.domain;
 
+import orangetaxiteam.cocoman.domain.exceptions.BadRequestException;
+import orangetaxiteam.cocoman.domain.exceptions.ErrorCode;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ContentsRepository extends JpaRepository<Contents, String> {
+    Optional<Contents> findByTitle(String title);
+
+    default Contents findByTitleOrElseThrow(String title) {
+        return this.findByTitle(title).orElseThrow(
+                () -> new BadRequestException(ErrorCode.ROW_DOES_NOT_EXIST, "invalid contents title")
+        );
+    }
 }
